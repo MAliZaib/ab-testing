b01b243a5f96c6e75047259009be1c4fe43c0a64
Ali Zaib <alizaib.alphasquad@gmail.com>
2021-06-28 22:25:42 +0500

    component

--- pages/Page1.js
+++ pages/Page1.js
@@ -5,17 +5,17 @@ import styles from '../styles/Home.module.css'
 const Page1 = () => {
     return(
 
-        <div class="md:mx-auto md:container px-4">
-            <div class="pt-10 md:pt-40">
-                <div class="container mx-auto">
-                    <div class="flex flex-wrap items-center pb-12">
-                        <div class="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
-                            <div class="py-2 text-color">
-                                <h1 role="heading" class="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black">Boost adoption of product and services !!!!!</h1>
-                                <p role="contentinfo" class="text-lg lg:text-3xl lg:leading-7 md:leading-10 f-f-r py-4 md:py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</p>
-                                <button class="flex items-center cursor-pointer pb-4 md:pb-0 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
-                                    <span aria-label="lets get started" role="button" class="focus outline-none f-f-r text-lg lg:text-2xl font-semibold underline text-indigo-700">Lets Get Started</span>
-                                    <div class="pl-2">
+        <div className="md:mx-auto md:container px-4">
+            <div className="pt-10 md:pt-40">
+                <div className="container mx-auto">
+                    <div className="flex flex-wrap items-center pb-12">
+                        <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
+                            <div className="py-2 text-color">
+                                <h1 role="heading" className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black">Boost adoption of product and services !!!!!</h1>
+                                <p role="contentinfo" className="text-lg lg:text-3xl lg:leading-7 md:leading-10 f-f-r py-4 md:py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</p>
+                                <button className="flex items-center cursor-pointer pb-4 md:pb-0 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
+                                    <span aria-label="lets get started" role="button" className="focus outline-none f-f-r text-lg lg:text-2xl font-semibold underline text-indigo-700">Lets Get Started</span>
+                                    <div className="pl-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                             <path d="M13.1719 12L8.22192 7.04999L9.63592 5.63599L15.9999 12L9.63592 18.364L8.22192 16.95L13.1719 12Z" fill="#D53F8C"></path>
                                         </svg>
@@ -23,13 +23,13 @@ const Page1 = () => {
                                 </button>
                             </div>
                         </div>
-                        <div class="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                            <img class="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://cdn.tuk.dev/assets/templates/prodify/ProductAdoption.png" alt="A girl enjoying in sunlight" role="img" />
-                            <div class="relative z-10 bg-white rounded shadow p-6 w-10/12 -mb-20">
-                                <div class="flex items-center justify-between w-full sm:w-full mb-8">
-                                    <div class="flex items-center">
-                                        <div class="p-4 bg-yellow-200 rounded-md">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-discount" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                            <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://cdn.tuk.dev/assets/templates/prodify/ProductAdoption.png" alt="A girl enjoying in sunlight" role="img" />
+                            <div className="relative z-10 bg-white rounded shadow p-6 w-10/12 -mb-20">
+                                <div className="flex items-center justify-between w-full sm:w-full mb-8">
+                                    <div className="flex items-center">
+                                        <div className="p-4 bg-yellow-200 rounded-md">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-discount" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                 <path stroke="none" d="M0 0h24v24H0z"></path>
                                                 <line x1="9" y1="15" x2="15" y2="9"></line>
                                                 <circle cx="9.5" cy="9.5" r=".5"></circle>
@@ -37,43 +37,43 @@ const Page1 = () => {
                                                 <path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7a2.2 2.2 0 0 0 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1a2.2 2.2 0 0 0 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55 v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55 v-1"></path>
                                             </svg>
                                         </div>
-                                        <div class="ml-6">
-                                            <p class="mb-1 leading-5 text-gray-800 font-bold text-2xl">2,330</p>
-                                            <p class="text-gray-600 text-sm tracking-normal font-normal leading-5">Avg Sales</p>
+                                        <div className="ml-6">
+                                            <p className="mb-1 leading-5 text-gray-800 font-bold text-2xl">2,330</p>
+                                            <p className="text-gray-600 text-sm tracking-normal font-normal leading-5">Avg Sales</p>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="flex items-center pl-3 text-green-400">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trending-up text-green-700" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <div className="flex items-center pl-3 text-green-400">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trending-up text-green-700" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                 <path stroke="none" d="M0 0h24v24H0z"></path>
                                                 <polyline points="3 17 9 11 13 15 21 7"></polyline>
                                                 <polyline points="14 7 21 7 21 14"></polyline>
                                             </svg>
-                                            <p class="text-green-700 text-xs tracking-wide font-bold leading-normal pl-1">7.2%</p>
+                                            <p className="text-green-700 text-xs tracking-wide font-bold leading-normal pl-1">7.2%</p>
                                         </div>
-                                        <p class="font-normal text-xs text-right leading-4 text-green-700 tracking-normal">Increase</p>
+                                        <p className="font-normal text-xs text-right leading-4 text-green-700 tracking-normal">Increase</p>
                                     </div>
                                 </div>
-                                <div class="relative mb-3">
-                                    <hr class="h-1 rounded-sm bg-gray-200" />
-                                    <hr class="absolute top-0 h-1 w-7/12 rounded-sm bg-indigo-700" />
+                                <div className="relative mb-3">
+                                    <hr className="h-1 rounded-sm bg-gray-200" />
+                                    <hr className="absolute top-0 h-1 w-7/12 rounded-sm bg-indigo-700" />
                                 </div>
-                                <h2 class="text-base text-gray-600 font-normal tracking-normal leading-5">Yearly Goal</h2>
+                                <h2 className="text-base text-gray-600 font-normal tracking-normal leading-5">Yearly Goal</h2>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="pb-32 pt-16">
-                <div class="mx-auto">
-                    <div class="flex flex-wrap flex-row-reverse items-center">
-                        <div class="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
-                            <div class="py-2 text-color">
-                                <div><h1 role="heading" class="text-2xl lg:text-6xl tracking-tighter md:leading-snug f-f-l font-black">Easily manage sales through our invoicing system</h1></div>
-                                <p role="contentinfo" class="text-lg lg:text-3xl leading-7 md:leading-10 f-f-r py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</p>
-                                <button class="flex items-center cursor-pointer pb-4 md:pb-0 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
-                                    <span aria-label="lets get started" role="button" class="focus outline-none f-f-r text-lg lg:text-2xl font-semibold underline text-indigo-700">Lets Get Started</span>
-                                    <div class="pl-2">
+            <div className="pb-32 pt-16">
+                <div className="mx-auto">
+                    <div className="flex flex-wrap flex-row-reverse items-center">
+                        <div className="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
+                            <div className="py-2 text-color">
+                                <div><h1 role="heading" className="text-2xl lg:text-6xl tracking-tighter md:leading-snug f-f-l font-black">Easily manage sales through our invoicing system</h1></div>
+                                <p role="contentinfo" className="text-lg lg:text-3xl leading-7 md:leading-10 f-f-r py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</p>
+                                <button className="flex items-center cursor-pointer pb-4 md:pb-0 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
+                                    <span aria-label="lets get started" role="button" className="focus outline-none f-f-r text-lg lg:text-2xl font-semibold underline text-indigo-700">Lets Get Started</span>
+                                    <div className="pl-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                             <path d="M13.1719 12L8.22192 7.04999L9.63592 5.63599L15.9999 12L9.63592 18.364L8.22192 16.95L13.1719 12Z" fill="#D53F8C"></path>
                                         </svg>
@@ -81,32 +81,32 @@ const Page1 = () => {
                                 </button>
                             </div>
                         </div>
-                        <div class="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                            <img class="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://cdn.tuk.dev/assets/templates/prodify/invoicing-system.png" alt="A group of three having a meeting" role="img" />
-                            <div class="relative z-10 p-4 bg-white shadow rounded mx-auto w-9/12 -mb-20">
-                                <p class="text-gray-800 font-normal text-md leading-4 tracking-normal mb-2">Invoice # 35RD87</p>
-                                <p class="text-gray-600 text-xs leading-3 font-normal">Assigned to: Josh Rollins</p>
-                                <div class="mt-4 flex items-center">
-                                    <span class="text-gray-600">
-                                        <svg class="feather feather-user" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                        <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                            <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://cdn.tuk.dev/assets/templates/prodify/invoicing-system.png" alt="A group of three having a meeting" role="img" />
+                            <div className="relative z-10 p-4 bg-white shadow rounded mx-auto w-9/12 -mb-20">
+                                <p className="text-gray-800 font-normal text-md leading-4 tracking-normal mb-2">Invoice # 35RD87</p>
+                                <p className="text-gray-600 text-xs leading-3 font-normal">Assigned to: Josh Rollins</p>
+                                <div className="mt-4 flex items-center">
+                                    <span className="text-gray-600">
+                                        <svg className="feather feather-user" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
                                             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                             <circle cx="12" cy="7" r="4"></circle>
                                         </svg>
                                     </span>
-                                    <p class="ml-1 text-sm text-gray-800 leading-3 tracking-normal font-normal">Specter Consultancy</p>
+                                    <p className="ml-1 text-sm text-gray-800 leading-3 tracking-normal font-normal">Specter Consultancy</p>
                                 </div>
-                                <div class="mt-3 flex items-center">
-                                    <span class="text-gray-600">
-                                        <svg class="feather feather-dollar-sign" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                                <div className="mt-3 flex items-center">
+                                    <span className="text-gray-600">
+                                        <svg className="feather feather-dollar-sign" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
                                             <line x1="12" y1="1" x2="12" y2="23"></line>
                                             <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
                                         </svg>
                                     </span>
-                                    <p class="ml-1 text-sm text-gray-800 leading-3 tracking-normal font-normal">2,354 USD</p>
+                                    <p className="ml-1 text-sm text-gray-800 leading-3 tracking-normal font-normal">2,354 USD</p>
                                 </div>
-                                <button class="relative focus:outline-none mt-4 py-2 pr-10 bg-indigo-700 text-white tracking-normal text-xs pl-3 cursor-pointer hover:opacity-90 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700" role="button">
+                                <button className="relative focus:outline-none mt-4 py-2 pr-10 bg-indigo-700 text-white tracking-normal text-xs pl-3 cursor-pointer hover:opacity-90 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700" role="button">
                                     View Invoice
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="absolute inset-0 m-auto mr-3 icon icon-tabler icon-tabler-arrow-right" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-0 m-auto mr-3 icon icon-tabler icon-tabler-arrow-right" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z"></path>
                                         <line x1="5" y1="12" x2="19" y2="12"></line>
                                         <line x1="13" y1="18" x2="19" y2="12"></line>
